7d38f8a6851e6fe289abf2612377adf2342e6910
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-14 13:55:15 +0200

    serverside valmis

---
+++ serverside.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ServerSide</h1>
+    <p>
+        <?php
+        for($row=1; $row<5; $row++){
+          echo 'Rivi = '.$row.'<br>';  
+        }
+        ?>
+    </p>
+</body>
+</html>
